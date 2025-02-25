e48ccdf00c0258d5f0f22536ac4b26aa4e941539
TheRealSlimSchaali <patrick.schaal@rwth-aachen.de>
2025-02-25 17:53:52 +0100

    simpler init

--- custom_components/symptom_diary/__init__.py
+++ custom_components/symptom_diary/__init__.py
@@ -6,26 +6,9 @@ from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import discovery
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Symptom Diary integration asynchronously."""
-    # Register the sensor with Home Assistant
-    hass.data[DOMAIN] = {}
+async def async_setup(hass, config):
+    hass.states.async_set("symptom_diary.world", "Some Symptom")
 
-    # You can also set up any necessary services or configurations here
-
+    # Return boolean to indicate that initialization was successful.
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEntry):
-    """Set up a config entry for the Symptom Diary integration."""
-    # Create an instance of the sensor
-    sensor = SymptomDiarySensor()
-
-    # Add the sensor to Home Assistant
-    hass.data[DOMAIN][entry.entry_id] = sensor
-
-    # Register the sensor with Home Assistant
-    async_add_entities([sensor])
-
-    return True
-
-# You can also include any necessary imports or setup code here
